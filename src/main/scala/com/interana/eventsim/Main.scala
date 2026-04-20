613fd2beeba85d676cc7adb3d5299ccb112cad1d
megawatiw03 <megawatiw03@gmail.com>
2026-04-20 14:43:10 +0700

    added StringSerializer class

--- src/main/scala/com/interana/eventsim/Main.scala
+++ src/main/scala/com/interana/eventsim/Main.scala
@@ -9,6 +9,7 @@ import com.interana.eventsim.Utilities.{SimilarSongParser, TrackListenCount}
 import com.interana.eventsim.buildin.{DeviceProperties, UserProperties}
 import com.interana.eventsim.config.ConfigFromFile
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig}
+import org.apache.kafka.common.serialization.StringSerializer
 import org.rogach.scallop.{ScallopOption, ScallopConf}
 
 import scala.collection.mutable
@@ -120,9 +121,9 @@ object Main extends App {
 
   val kafkaProducer = if (ConfFromOptions.kafkaBrokerList.isDefined) {
     val kafkaProperties = new Properties()
-    kafkaProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, ConfFromOptions.kafkaBrokerList.get.get);
-    kafkaProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-    kafkaProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+    kafkaProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, ConfFromOptions.kafkaBrokerList.get.get)
+    kafkaProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class)
+    kafkaProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class)
     // val producerConfig = new ProducerConfig(kafkaProperties)
     new Some(new KafkaProducer[Array[Byte],Array[Byte]](kafkaProperties))
   } else None
